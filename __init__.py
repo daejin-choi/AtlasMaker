9da838067e6b0e693529885c235a9e802b9dd87c
Choi Daejin <soyenze@Choi-Daejins-MacBook-Pro.local>
2011-10-26 02:25:18 +0900

    add init-contents

--- __init__.py
+++ __init__.py
@@ -5,6 +5,13 @@ Actually __main__.py works
 Version: 1.0b
 Clone Address : git clone git@github.com:ForElise/AtlasMaker.git
 
+Dependency
+1.pypng ( need more than python 2.4 )
+ - download
+   : svn checkout http://pypng.googlecode.com/svn/trunk/ pypng-read-only
+ - install
+   : python setup.py install
+
 Usage
 1. implement directly
   python Atlasmaker [location folder] [final size]
